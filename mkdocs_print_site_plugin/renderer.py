ddfadf8ae53ea653a4b38ea0f97421e61eee6639
Tim Vink <vinktim@gmail.com>
2020-12-17 20:34:58 +0100

    Clarify avoiding system print diaglog in print-site box, closes #33

--- mkdocs_print_site_plugin/renderer.py
+++ mkdocs_print_site_plugin/renderer.py
@@ -118,7 +118,8 @@ class Renderer(object):
                 <em>This box will disappear when printing</em>
                 <span style="float: right"><a href="https://timvink.github.io/mkdocs-print-site-plugin/">mkdocs-print-site-plugin</a></span>
             </p>
-            <p>This page combines all site pages and applies print and PDF friendly styling. Print or export to PDF using <b>File > Print > Save as PDF</b></p>
+            <p>This page combines all site pages and applies print and PDF friendly styling. Print or export to PDF using <b>File > Print > Save as PDF</b>.</p>
+            <p>For creating PDFs, make sure to use the browser's print and save-as-pdf function instead of the system dialog.</p>
         </div>
         """
 
