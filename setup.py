b6226a3dade007bfbeff265214b40377cd0f79a4
Tim Vink <vinktim@gmail.com>
2021-05-10 16:33:53 +0200

    Bump mkdocs-material requirement

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ setup(
         "Topic :: Documentation",
         "Topic :: Text Processing",
     ],
-    install_requires=["mkdocs>=1.0", "mkdocs-material>=6.1.0"],
+    install_requires=["mkdocs>=1.1", "mkdocs-material>=7.1.0"],
     packages=find_packages(),
     entry_points={"mkdocs.plugins": ["print-site = mkdocs_print_site_plugin.plugin:PrintSitePlugin"]},
 )
