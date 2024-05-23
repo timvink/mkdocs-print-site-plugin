0a7300b251929d0c06d5f719a741525cf0ec26f8
Tim Vink <vinktim@gmail.com>
2024-05-23 17:32:17 +0000

    Support python 3.12

--- setup.py
+++ setup.py
@@ -29,6 +29,7 @@ setup(
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "License :: OSI Approved :: MIT License",
         "Topic :: Documentation",
         "Topic :: Text Processing",
